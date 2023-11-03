0cde0d21d1bb685a34e76035e99373c8b555b190
Lucía Costamagna <luci3220@hotmail.com>
2023-11-02 21:16:25 -0300

    add animation box

--- animation/App.js
+++ animation/App.js
@@ -2,17 +2,19 @@ import { StyleSheet, Text, View } from "react-native";
 import { Bnt } from "./Components/Bnt";
 import Sequence from "./Components/Sequence";
 import { FontAwesome } from "@expo/vector-icons";
+import ReanimatedExamples from "./Components/ReanimatedExamples";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>ANIMATIONS</Text>
+    <ReanimatedExamples/>
+    // <View style={styles.container}>
+    //   <Text style={styles.text}>ANIMATIONS</Text> */}
 
-      {/* <Bnt title={"Press me"} /> */}
-      {/* <Sequence>
-        <FontAwesome name="bell" size={50} color="gold" />
-      </Sequence> */}
-    </View>
+    //   <Bnt title={"Press me"} />
+    //   <Sequence>
+    //     <FontAwesome name="bell" size={50} color="gold" />
+    //   </Sequence>
+    // </View>
   );
 }
 
--- animation/Components/ReanimatedExamples.js
+++ animation/Components/ReanimatedExamples.js
@@ -1,13 +1,83 @@
 //shared value
-
+import Animated, { useSharedValue, withTiming, Easing,useAnimatedStyle, withSpring} from 'react-native-reanimated'
 import React from 'react'
-import { SafeAreaView, ScrollView } from 'react-native'
+import { Button, SafeAreaView, ScrollView } from 'react-native'
+
+
+
+function SharedValue () {
+    const randomWidth = useSharedValue(10);
+
+    const config = {
+        duration: 500,
+        easing: Easing.bezier(0.5, 0.01, 0, 1),
+      };
+
+      const style= useAnimatedStyle(()=>{
+        return {
+          with:withTiming(randomWidth.value, config)
+        }
+      })
+
+
+    return(
+        <>
+        <Animated.View
+        style={[
+            {height:30, backgroundColor:'yellow', marginTop:80, margin:10},
+              style
+
+        ]}/>
+        <Button
+        title='Toggle'
+        onPress={()=>{
+          randomWidth.value= Math.random() * 350;
+        }}
+        />
+        </>
+    )
+}
+
+
+function Box(){
+   const offset = useSharedValue(0);
+
+   const animatedStyles= useAnimatedStyle(()=>{
+     return {
+     transform:[{
+      translateX:withSpring(offset.value * 255, {
+        damping:40,
+        stiffness:90,
+      })
+
+
+      }]
+     }
+  })
+  return(
+<>
+<Animated.View
+
+style={[
+  {width:100, height:100, backgroundColor:'purple'},
+  animatedStyles
+]}
+/>
+<Button title="Move"
+onPress={()=> offset.value = withSpring(Math.random())}
+/>
+</>
+  )
+}
+
+
 
 const ReanimatedExamples = () => {
   return (
     <SafeAreaView>
         <ScrollView>
-            
+       <SharedValue/>
+       <Box/>
         </ScrollView>
     </SafeAreaView>
   )
